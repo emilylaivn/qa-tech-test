0b8d9ca4200a561ca97d7ac6b54429a4a3db29f0
Yair <yabraham@educationperfect.com>
2021-05-19 14:31:33 +1200

    Add test scenarios and data set up command

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,11 +1,51 @@
-describe('My First Test', () => {
+describe('Todo list test', () => {
     beforeEach(() => {
-        cy.visit('http://127.0.0.1:8080/')
-    })
+        const todoItems = [
+            {"title": "Item 1", "completed": false}, 
+            {"title": "Item 2", "completed": true}, 
+            {"title": "Item 3", "completed": false}];
 
-    it('.focus() - focus on a DOM element', () => {
-        cy.get('.new-todo').focus()
-            .type('fake@email.com').should('have.value', 'fake@email.com')
-            .type('{enter}')
-    })
+        cy.loadData(todoItems);
+        cy.visit('http://127.0.0.1:8080/');
+    });
+
+    it('should enter a new todo', () => {
+        cy.get('.new-todo').type('fake@email.com{enter}');
+        cy.get('.todo').should('contain.text', 'fake@email.com');
+    });
+
+    it('should filter items', () => {
+        // "Active" should only display active items
+        cy.contains("Active").click();
+        cy.get("li.todo").each((todo) => {
+            cy.wrap(todo).should("not.have.class", "completed")
+        });
+
+        // "Completed" should only display completed items
+        cy.contains("Completed").click();
+        cy.get("li.todo").each((todo) => {
+            cy.wrap(todo).should("have.class", "completed")
+        });
+    });
+
+    it('should mark a todo as completed', () => {
+        // Mark "Item 3" as completed
+    });
+
+    it('should toggle all items', () => {
+        // ".toggle-all" should mark all items as completed
+        // and unmark all items when clicked a second time
+    });
+
+    it('should display correct remaining items', () => {
+        // "x items left" should update correctly
+    });
+
+    it('should clear completed items', () => {
+        // "Clear completed" removes completed items
+    });
+
+    it('should delete an item', () => {
+        // Delete an item whether it is completed or not
+    });
 })
